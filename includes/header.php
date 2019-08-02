e84f286a6226e64ec245a2d79bebdb4c23414408
Hana Gabrielle Bidon <37670365+hrb56@users.noreply.github.com>
2019-08-02 17:31:27 -0400

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,13 @@
+ <header>
+    <h1 id="title"><?php echo $title; ?></h1>
+    <h2>Mental Health Education</h2>
+
+    <nav id="menu">
+      <ul>
+        <li class = "<?php echo $HOME ?>"><a href="index.php">Home</a></li>
+        <li class = "<?php echo $MENTAL_HEALTH ?>"><a href="mentalhealth.php">Mental Health & Mental Illnesses</a></li>
+        <li class = "<?php echo $CRISIS ?>"><a href="crisis.php">How to help in a Crisis</a></li>
+        <li class = "<?php echo $HELP ?>"><a href = "help.php">Seek Out Help</a></li>
+      </ul>
+    </nav>
+  </header>
